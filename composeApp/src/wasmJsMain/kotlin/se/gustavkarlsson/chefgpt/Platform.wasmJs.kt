895127abecfd3d46f25bb99d0a18b67cebbb9bf9
Gustav Karlsson <gustav.karlsson@gmail.com>
2026-02-18 17:37:13 +0100

    Remove the platform-specific example code

--- composeApp/src/wasmJsMain/kotlin/se/gustavkarlsson/chefgpt/Platform.wasmJs.kt
+++
@@ -1,7 +0,0 @@
-package se.gustavkarlsson.chefgpt
-
-class WasmPlatform : Platform {
-    override val name: String = "Web with Kotlin/Wasm"
-}
-
-actual fun getPlatform(): Platform = WasmPlatform()
