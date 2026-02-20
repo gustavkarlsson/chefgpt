0d4efe8e7f4e7796129882a196d7436727a5a507
Gustav Karlsson <gustav.karlsson@epidemicsound.com>
2026-02-20 18:22:05 +0100

    Add getRecipeInformation to SpoonacularClient

--- server/src/main/kotlin/se/gustavkarlsson/chefgpt/tools/SpoonacularClient.kt
+++ server/src/main/kotlin/se/gustavkarlsson/chefgpt/tools/SpoonacularClient.kt
@@ -32,6 +32,20 @@ data class Ingredient(
 )
 
 @Serializable
+data class ExtendedIngredient(
+    val aisle: String,
+    val amount: Double,
+    val consistency: String,
+    val id: Int,
+    val image: String,
+    val meta: List<String>,
+    val name: String,
+    val original: String,
+    val originalName: String,
+    val unit: String,
+)
+
+@Serializable
 data class Recipe(
     val id: Int,
     val image: String,
@@ -43,6 +57,32 @@ data class Recipe(
     val unusedIngredients: List<Ingredient>,
     val usedIngredientCount: Int,
     val usedIngredients: List<Ingredient>,
+)
+
+@Serializable
+data class RecipeDetails(
+    val id: Int,
+    val title: String,
+    val image: String,
+    val servings: Int,
+    val readyInMinutes: Int,
+    val sourceUrl: String,
+    val spoonacularSourceUrl: String,
+    val healthScore: Double,
+    val spoonacularScore: Double,
+    val pricePerServing: Double,
+    val cheap: Boolean,
+    val dairyFree: Boolean,
+    val glutenFree: Boolean,
+    val ketogenic: Boolean? = null,
+    val lowFodmap: Boolean,
+    val sustainable: Boolean,
+    val vegan: Boolean,
+    val vegetarian: Boolean,
+    val whole30: Boolean? = null,
+    val extendedIngredients: List<ExtendedIngredient>,
+    val summary: String,
+    val instructions: String? = null,
 )
 
 class SpoonacularClient(
@@ -66,10 +106,13 @@ class SpoonacularClient(
 
     @Tool
     @LLMDescription(
-        "Find recipes that use as many of the given ingredients as possible and require as few additional ingredients as possible. ingredients: Ingredients that you have at home. resultCount: The maximum number of results to return (1-100).",
+        "Find recipes that use as many of the given ingredients as possible" +
+            "and require as few additional ingredients as possible.",
     )
     suspend fun findByIngredients(
+        @LLMDescription("Ingredients that you have at home")
         ingredients: List<String>,
+        @LLMDescription("The maximum number of results to return (1-100)")
         resultCount: Int,
     ): List<Recipe> =
         client
@@ -80,6 +123,35 @@ class SpoonacularClient(
                 parameter("number", resultCount)
                 parameter("ranking", 2)
                 parameter("ignorePantry", true)
+            }.body()
+
+    @Tool
+    @LLMDescription(
+        "Use a recipe id to get full information about a recipe," +
+            "such as ingredients, nutrition, diet and allergen information, etc.",
+    )
+    suspend fun getRecipeInformation(
+        @LLMDescription("The id of the recipe.")
+        id: Int,
+        @LLMDescription(
+            "Include nutrition data in the recipe information." +
+                "Nutrition data is per serving." +
+                "If you want the nutrition data for the entire recipe," +
+                "just multiply by the number of servings.",
+        )
+        includeNutrition: Boolean = false,
+        @LLMDescription("Add a wine pairing to the recipe.")
+        addWinePairing: Boolean = false,
+        @LLMDescription("Add taste data to the recipe.")
+        addTasteData: Boolean = false,
+    ): RecipeDetails =
+        client
+            .get("https://api.spoonacular.com/recipes/$id/information") {
+                header("x-api-key", apiKey)
+
+                parameter("includeNutrition", includeNutrition)
+                parameter("addWinePairing", addWinePairing)
+                parameter("addTasteData", addTasteData)
             }.body()
 
     override fun close() {
