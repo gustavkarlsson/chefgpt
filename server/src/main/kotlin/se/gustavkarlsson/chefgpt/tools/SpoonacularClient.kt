0a6dcf76a99ce8ff95fbd33340a006947f9b1a23
Gustav Karlsson <gustav.karlsson@epidemicsound.com>
2026-02-20 18:29:02 +0100

    Make the API return a string

--- server/src/main/kotlin/se/gustavkarlsson/chefgpt/tools/SpoonacularClient.kt
+++ server/src/main/kotlin/se/gustavkarlsson/chefgpt/tools/SpoonacularClient.kt
@@ -7,83 +7,9 @@ import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.plugins.HttpTimeout
-import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.request.get
 import io.ktor.client.request.header
 import io.ktor.client.request.parameter
-import io.ktor.serialization.kotlinx.json.json
-import kotlinx.serialization.Serializable
-import kotlinx.serialization.json.Json
-
-@Serializable
-data class Ingredient(
-    val aisle: String,
-    val amount: Double,
-    val id: Int,
-    val image: String,
-    val meta: List<String>,
-    val name: String,
-    val original: String,
-    val originalName: String,
-    val unit: String,
-    val unitLong: String,
-    val unitShort: String,
-    val extendedName: String? = null,
-)
-
-@Serializable
-data class ExtendedIngredient(
-    val aisle: String,
-    val amount: Double,
-    val consistency: String,
-    val id: Int,
-    val image: String,
-    val meta: List<String>,
-    val name: String,
-    val original: String,
-    val originalName: String,
-    val unit: String,
-)
-
-@Serializable
-data class Recipe(
-    val id: Int,
-    val image: String,
-    val imageType: String,
-    val likes: Int,
-    val missedIngredientCount: Int,
-    val missedIngredients: List<Ingredient>,
-    val title: String,
-    val unusedIngredients: List<Ingredient>,
-    val usedIngredientCount: Int,
-    val usedIngredients: List<Ingredient>,
-)
-
-@Serializable
-data class RecipeDetails(
-    val id: Int,
-    val title: String,
-    val image: String,
-    val servings: Int,
-    val readyInMinutes: Int,
-    val sourceUrl: String,
-    val spoonacularSourceUrl: String,
-    val healthScore: Double,
-    val spoonacularScore: Double,
-    val pricePerServing: Double,
-    val cheap: Boolean,
-    val dairyFree: Boolean,
-    val glutenFree: Boolean,
-    val ketogenic: Boolean? = null,
-    val lowFodmap: Boolean,
-    val sustainable: Boolean,
-    val vegan: Boolean,
-    val vegetarian: Boolean,
-    val whole30: Boolean? = null,
-    val extendedIngredients: List<ExtendedIngredient>,
-    val summary: String,
-    val instructions: String? = null,
-)
 
 class SpoonacularClient(
     private val apiKey: String,
@@ -92,15 +18,7 @@ class SpoonacularClient(
     private val client =
         HttpClient(CIO) {
             install(HttpTimeout) {
-                requestTimeoutMillis = 1000
-            }
-            install(ContentNegotiation) {
-                json(
-                    Json {
-                        ignoreUnknownKeys = true
-                        isLenient = true
-                    },
-                )
+                requestTimeoutMillis = 5000
             }
         }
 
@@ -114,7 +32,7 @@ class SpoonacularClient(
         ingredients: List<String>,
         @LLMDescription("The maximum number of results to return (1-100)")
         resultCount: Int,
-    ): List<Recipe> =
+    ): String =
         client
             .get("https://api.spoonacular.com/recipes/findByIngredients") {
                 header("x-api-key", apiKey)
@@ -144,7 +62,7 @@ class SpoonacularClient(
         addWinePairing: Boolean = false,
         @LLMDescription("Add taste data to the recipe.")
         addTasteData: Boolean = false,
-    ): RecipeDetails =
+    ): String =
         client
             .get("https://api.spoonacular.com/recipes/$id/information") {
                 header("x-api-key", apiKey)
