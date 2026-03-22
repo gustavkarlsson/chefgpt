8b0e7996763a7bd1eadc3ec927e530e6bcff069e
Gustav Karlsson <gustav.karlsson@gmail.com>
2026-03-22 16:41:32 +0100

    Add TODO about ChatId API model

--- server/src/main/kotlin/se/gustavkarlsson/chefgpt/chats/ChatId.kt
+++ server/src/main/kotlin/se/gustavkarlsson/chefgpt/chats/ChatId.kt
@@ -2,6 +2,7 @@ package se.gustavkarlsson.chefgpt.chats
 
 import kotlin.uuid.Uuid
 
+// TODO Make serializable (as primitive string) and add to shared API models. Use directly in server and client instead of unwrapping/wrapping the value
 @JvmInline
 value class ChatId(
     val value: Uuid,
