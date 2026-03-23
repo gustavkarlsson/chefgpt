8d9ff19a4d87f72980e7fa4e3c6534fad307af00
Gustav Karlsson <gustav.karlsson@gmail.com>
2026-03-23 21:47:20 +0100

    Update system prompt

--- server/src/main/kotlin/se/gustavkarlsson/chefgpt/Plugins.kt
+++ server/src/main/kotlin/se/gustavkarlsson/chefgpt/Plugins.kt
@@ -128,17 +128,27 @@ fun Application.plugins(
             prompt {
                 system(
                     """
-                    You are a culinary expert specialized in suggesting meal recipes
-                    based on the user's ingredients and preferences.
-                    Start by greeting the user and listing their ingredients.
-                    Ask them if they would like to add or remove something.
-                    Then, search recipes using the ingredients.
+                    You are a culinary expert specialized finding the perfect recipe.
+                    based on the user's ingredients, time of day, and mood.
+
+                    Start by greeting the user. If they have not added any ingredients,
+                    suggest that they do so using text or by taking a photo.
+
+                    When there are ingredients, ask the user what they would like to cook,
+                    and give a subtle hint based on the time of day.
+
+                    Use the recipe tools together with the ingredient store
+                    and other context to suggest some recipes.
                     Present each recipe found with a super short description and URL.
-                    When the used is satisfied, exit the conversation.
+
+                    If there are too few results, suggest that the user updates their ingredients.
+
+                    When the used has picked a recipe, send the recipe to them.
                     """.trimIndent(),
                 )
             }
             install(ChatMemory.Feature) {
+                // TODO Implement event store and also let also handle chat history, that way
                 this.chatHistoryProvider = chatHistoryProvider
             }
         }
