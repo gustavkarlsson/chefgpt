53462bdfd5c84a4223277e6945101567f877143f
Gustav Karlsson <gustav.karlsson@epidemicsound.com>
2026-03-29 18:43:17 +0200

    Fix DB connection

--- server/src/main/kotlin/se/gustavkarlsson/chefgpt/db/ConnectR2dbcDatabase.kt
+++ server/src/main/kotlin/se/gustavkarlsson/chefgpt/db/ConnectR2dbcDatabase.kt
@@ -3,7 +3,19 @@ package se.gustavkarlsson.chefgpt.db
 import io.ktor.server.config.ApplicationConfig
 import org.jetbrains.exposed.v1.r2dbc.R2dbcDatabase
 
-fun connectR2dbcDatabase(config: ApplicationConfig): R2dbcDatabase {
-    val url = getDatabaseUrl("r2dbc", config)
-    return R2dbcDatabase.connect(url)
-}
+fun connectR2dbcDatabase(config: ApplicationConfig): R2dbcDatabase =
+    R2dbcDatabase.connect(
+        url = getUrl(config),
+        user = config.property("username").getString(),
+        password = config.property("password").getString(),
+    )
+
+private fun getUrl(config: ApplicationConfig): String =
+    buildString {
+        append("r2dbc:postgresql://")
+        append(config.property("host").getString())
+        append(":")
+        append(config.property("port").getString())
+        append("/")
+        append(config.property("name").getString())
+    }
--- server/src/main/kotlin/se/gustavkarlsson/chefgpt/db/MigrateDatabase.kt
+++ server/src/main/kotlin/se/gustavkarlsson/chefgpt/db/MigrateDatabase.kt
@@ -15,8 +15,22 @@ fun migrateDatabase(config: ApplicationConfig) {
 }
 
 private fun createSimpleDataSource(config: ApplicationConfig): DataSource {
-    val url = getDatabaseUrl("jdbc", config)
+    val url = getUrl(config)
     return PGSimpleDataSource().apply {
         setUrl(url)
     }
 }
+
+private fun getUrl(config: ApplicationConfig): String =
+    buildString {
+        append("jdbc:postgresql://")
+        append(config.property("host").getString())
+        append(":")
+        append(config.property("port").getString())
+        append("/")
+        append(config.property("name").getString())
+        append("?user=")
+        append(config.property("username").getString())
+        append("&password=")
+        append(config.property("password").getString())
+    }
