eeae10689ced334782b938f425bbf5552e85801d
Gustav Karlsson <gustav.karlsson@epidemicsound.com>
2026-04-10 23:35:52 +0200

    Stream chats from client

--- app/src/commonMain/kotlin/se/gustavkarlsson/chefgpt/chats/ApiChatRepository.kt
+++ app/src/commonMain/kotlin/se/gustavkarlsson/chefgpt/chats/ApiChatRepository.kt
@@ -5,10 +5,7 @@ import com.github.michaelbull.result.Result
 import com.github.michaelbull.result.map
 import com.github.michaelbull.result.onErr
 import com.github.michaelbull.result.onOk
-import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.map
 import se.gustavkarlsson.chefgpt.ChefGptClient
 import se.gustavkarlsson.chefgpt.ErrorResponse
@@ -21,13 +18,6 @@ private val log = Logger.withTag("${ApiChatRepository::class.simpleName}")
 class ApiChatRepository(
     private val client: ChefGptClient,
 ) : ChatRepository {
-    private val refreshFlow =
-        MutableSharedFlow<Unit>(
-            replay = 1,
-            onBufferOverflow = BufferOverflow.DROP_OLDEST,
-            extraBufferCapacity = 1,
-        ).apply { tryEmit(Unit) }
-
     override suspend fun create(sessionId: SessionId): Result<Chat, ErrorResponse> =
         client
             .createChat(sessionId)
@@ -35,19 +25,14 @@ class ApiChatRepository(
                 log.i { "Created chat: ${it.id}" }
             }.onErr {
                 log.e { "Failed to create chat" }
-            }.also { refreshFlow.emit(Unit) }
-            .map { it.toChat() }
+            }.map { it.toChat() }
 
-    // TODO Use a streaming endpoint
-    override suspend fun getAll(sessionId: SessionId): Flow<Result<List<Chat>, ErrorResponse>> =
-        refreshFlow
-            .map {
-                client
-                    .getAllChats(sessionId)
-                    .map { chats ->
-                        chats.map { it.toChat() }
-                    }
-            }.distinctUntilChanged()
+    override suspend fun stream(sessionId: SessionId): Flow<List<Chat>> =
+        client
+            .listenToChats(sessionId)
+            .map { chats ->
+                chats.map { it.toChat() }
+            }
 
     override suspend fun delete(
         sessionId: SessionId,
@@ -59,7 +44,7 @@ class ApiChatRepository(
                 log.i { "Deleted chat: $chatId" }
             }.onErr {
                 log.e { "Failed to delete chat: $chatId" }
-            }.also { refreshFlow.emit(Unit) }
+            }
 }
 
 private fun ApiChat.toChat(): Chat = Chat(id, createdAt)
